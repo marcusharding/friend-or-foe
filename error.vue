9999eeed608e4e20e956da23b98693491300d8f2
Marcus <marcus@thinkingbox.com>
2023-05-21 13:26:10 -0400

    [#0] - Error page

---
+++ error.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="error fade-in">
+        <h1>Dang it, this page doesnt exist.</h1>
+        <NuxtLink 
+            to="/" 
+            class="button"
+        >
+            Go home
+        </NuxtLink>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+
+.error {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+
+.button {
+    max-width: 200px;
+}
+
+h1 {
+    color: #ffffff;
+}
+
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,3 @@
+<template class="404-page">
+    <h1>Dang it, this page doesnt exist.</h1>
+</template>
